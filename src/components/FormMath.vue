760e8ff932952571e3a467832010161743821a25
Jack <jpick329@gmail.com>
2022-04-30 18:59:35 -0500

    created method to run payout math

--- src/components/FormMath.vue
+++ src/components/FormMath.vue
@@ -12,6 +12,33 @@ export default {
       totalPayout: 0,
     };
   },
+  methods: {
+    calculatePayout() {
+      let amount = 0;
+      if (this.odds >= 100) {
+        // wager was on underdog
+        if (this.typeOfCalc) {
+          amount = this.money * (this.odds / 100);
+        } else {
+          amount = this.money / (this.odds / 100);
+        }
+      } else {
+        // wager was negative odds, chose favorite
+        if (this.typeOfCalc) {
+          amount = this.money * (100 / (this.odds * -1));
+        } else {
+          amount = this.money * ((-1 * this.odds) / 100);
+        }
+      }
+      let roundedAmount = Math.round(amount * 100) / 100;
+      this.potentialWinnings = roundedAmount;
+
+      let moneyToFloat = parseFloat(this.money);
+      let total = roundedAmount + moneyToFloat;
+      let roundedTotal = Math.round(total * 100) / 100;
+      this.totalPayout = roundedTotal;
+    },
+  },
 };
 </script>
 
@@ -26,6 +53,7 @@ export default {
         Odds:
         <input v-model="odds" type="number" />
       </label>
+      <button @click="calculatePayout">Run It</button>
     </form>
     <div>
       <p v-if="money < 0.1">Wager must be 10 cents or more</p>
