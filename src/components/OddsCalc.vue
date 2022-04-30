1531e6ebd71e615d994d3f1aa3e270bb7d2c2b2f
Jack <jpick329@gmail.com>
2022-04-30 12:50:23 -0500

    Created odds calc page and toggle btn

---
+++ src/components/OddsCalc.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      amountWagered: true,
+      buttonText: "Wager",
+      buttonToggleId: "toggleId",
+    };
+  },
+  methods: {
+    toggle() {
+      this.amountWagered = !this.amountWagered;
+      this.buttonText = this.amountWagered ? "Wager" : "Desired Winnings";
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="calculator">
+    <div class="flex-container">
+      <p>Toggle button to switch between wager and amount you want to win</p>
+      <button @click="toggle" :id="buttonToggleId">{{ buttonText }}</button>
+    </div>
+    <form @submit.prevent="">
+      <input v-model="addBet" />
+      <button>Submit Wager</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.flex-container {
+  display: flex;
+  margin: 5px;
+  padding: 5px;
+}
+
+#toggleId {
+  margin: 15px;
+  background-color: green;
+  color: ivory;
+}
+</style>
