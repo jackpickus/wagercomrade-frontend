d789c3f50ab116b45410856d382a1838afbd2474
Jack <jpick329@gmail.com>
2022-04-30 17:14:21 -0500

    created form to input amount and odds

---
+++ src/components/FormMath.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      wagerTooSmall: true,
+      money: "",
+      odds: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="">
+      <label htmlFor="money">
+        Money:
+        <input v-model="money" />
+      </label>
+      <label htmlFor="odds">
+        Odds:
+        <input v-model="odds" />
+      </label>
+    </form>
+    <div>
+      <p v-if="wagerTooSmall">ERROR wager must be 10 cents or more</p>
+      <p v-else>Lets make some money!</p>
+      <p>{{ money }}</p>
+      <p>{{ odds }}</p>
+    </div>
+  </div>
+</template>
