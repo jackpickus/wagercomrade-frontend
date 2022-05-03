48bb85e35b6d45ff7f58b7bfb53f2ea7aa970abe
Jack <jpick329@gmail.com>
2022-05-02 21:19:38 -0500

    added form to submit bet

---
+++ src/components/BetForm.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  data() {
+    return {
+      newBet: {
+        theBet: "",
+        theOdds: 0,
+        units: 0,
+        status: "pending",
+        date: new Date(),
+      },
+    };
+  },
+  methods: {
+    placeBet() {
+      console.log(this.newBet);
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>My Wager Tracker</h1>
+  <div>
+    <form @submit.prevent="">
+      <label htmlFor="theBet">
+        The Bet:
+        <input
+          v-model="newBet.theBet"
+          type="text"
+          placeholder="e.g. Bulls -5"
+        />
+      </label>
+      <label htmlFor="theOdds">
+        The Odds:
+        <input v-model="newBet.theOdds" type="number" placeholder="-110" />
+      </label>
+      <label htmlFor="units">
+        Units:
+        <input v-model="newBet.units" type="number" placeholder="1" />
+      </label>
+      <button @click="placeBet">Place Bet</button>
+    </form>
+  </div>
+</template>
