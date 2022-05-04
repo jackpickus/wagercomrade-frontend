d82d2f1a11061f41174cf542df3a325451370772
Jack <jpick329@gmail.com>
2022-05-03 19:37:30 -0500

    created list to view bets placed

--- src/components/BetForm.vue
+++ src/components/BetForm.vue
@@ -1,19 +1,66 @@
 <script>
+let id = 0;
 export default {
   data() {
     return {
-      newBet: {
-        theBet: "",
-        theOdds: 0,
-        units: 0,
-        status: "pending",
-        date: new Date(),
+      lostClass: "lostTextClass",
+      wonClass: "wonTextClass",
+      pendingClass: "pendingTextClass",
+
+      id: id,
+      theBet: "",
+      theOdds: "",
+      units: 0,
+      status: "Pending",
+
+      // dummy data
+      myBets: [
+        {
+          id: id++,
+          theBet: "Suns -4.5",
+          theOdds: -110,
+          units: 1.1,
+          status: "Pending",
+          date: new Date(),
+        },
+        {
+          id: id++,
+          theBet: "Bulls -5",
+          theOdds: -110,
+          units: 1.1,
+          status: "Lost",
+          date: new Date(),
+        },
+        {
+          id: id++,
+          theBet: "Cubs ML",
+          theOdds: 200,
+          units: 1,
+          status: "Won",
+          date: new Date(),
+        },
+      ],
+      styleObject: {
+        color: "red",
       },
     };
   },
   methods: {
     placeBet() {
-      console.log(this.newBet);
+      if (this.units >= 0.1) {
+        let newBet = {
+          id: id++,
+          theBet: this.theBet,
+          theOdds: this.theOdds,
+          units: this.units,
+          status: this.status,
+          date: new Date(),
+        };
+        this.myBets.push(newBet);
+        this.theBet = "";
+        this.theOdds = "";
+        this.units = "";
+      }
     },
   },
 };
@@ -25,22 +72,39 @@ export default {
     <form @submit.prevent="">
       <label htmlFor="theBet">
         The Bet:
-        <input
-          v-model="newBet.theBet"
-          type="text"
-          placeholder="e.g. Bulls -5"
-        />
+        <input v-model="theBet" type="text" placeholder="e.g. Bulls -5" />
       </label>
       <label htmlFor="theOdds">
         The Odds:
-        <input v-model="newBet.theOdds" type="number" placeholder="-110" />
+        <input v-model="theOdds" type="number" placeholder="-110" />
       </label>
       <label htmlFor="units">
         Units:
-        <input v-model="newBet.units" type="number" placeholder="1" />
+        <input v-model="units" type="number" placeholder="1" />
       </label>
       <button @click="placeBet">Place Bet</button>
     </form>
+    <h3>My Bets</h3>
+    <div>
+      <ul>
+        <li v-for="bet in myBets" :key="bet.id">
+          {{ bet.theBet }} @ {{ bet.theOdds }} for {{ bet.units }} units
+          <p v-if="bet.status == 'Lost'">
+            Status:
+            <span :class="lostClass">{{ bet.status }}</span>
+          </p>
+          <p v-if="bet.status == 'Won'">
+            Status:
+            <span :class="wonClass">{{ bet.status }}</span>
+          </p>
+          <p v-if="bet.status == 'Pending'">
+            Status:
+            <span :class="pendingClass">{{ bet.status }}</span>
+            <button type="button">Update Status</button>
+          </p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
