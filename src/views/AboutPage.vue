3687d5393ba85bfdba990366ae71c9773deab40a
Jack <jpick329@gmail.com>
2022-09-23 16:04:02 -0500

    created about page

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>Thank you for visiting my site!</p>
+    <p>
+      Here I will post all of my sports wagers and update them. The inspiration
+      for this project is action networks wager tracker which is far better than
+      this. To learn more about me please visit my
+      <a href="http://jackpickus.com" target="_blank">website</a>
+    </p>
+  </div>
+</template>
