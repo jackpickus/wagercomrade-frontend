4e89f5a605d0b0cb7990870908f82cbad27fc56d
Jack <jpick329@gmail.com>
2022-09-23 16:04:20 -0500

    created home page to have OddsCalc component

---
+++ src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<script setup>
+import OddsCalc from "../components/OddsCalc.vue";
+</script>
+
+<template>
+  <main>
+    <OddsCalc />
+  </main>
+</template>
+
+<style>
+.pendingTextClass {
+  color: orange;
+}
+
+.wonTextClass {
+  color: green;
+}
+
+.lostTextClass {
+  color: red;
+}
+</style>
