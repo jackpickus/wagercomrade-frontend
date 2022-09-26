7aeaf5cd9bc1cf14312794f125b4b8ff3ffabb45
Jack <jpick329@gmail.com>
2022-09-26 10:29:06 -0500

    created login form

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
     <h1>Login</h1>
+    <form>
+      <label>Username: <input v-model="username" /></label>
+      <label>Password:<input v-model="password" type="password" /></label>
+      <button type="submit">Login</button>
+    </form>
   </div>
 </template>
