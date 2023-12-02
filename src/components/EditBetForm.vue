1de276e8212c4543931c0549d81029dde9043548
Jack <jpick329@gmail.com>
2023-12-02 11:31:34 -0600

    init edit bet form

---
+++ src/components/EditBetForm.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  props: {
+    id: Number,
+    theBet: String,
+    theOdds: Number,
+    theUnits: Number,
+    betStatus: String,
+  },
+  data() {
+    return {
+      editStatus: false,
+      newWager: "",
+      newOdds: "",
+      newUnits: 0,
+      newStatus: "",
+    };
+  },
+};
+</script>
+<template>
+  <div>
+    <h1>Edit Bet</h1>
+    <form @submit.prevent="">
+      <label>
+        <input v-model="newWager" />
+      </label>
+      <label>
+        <input v-model="newOdds" />
+      </label>
+      <label>
+        <input v-model="newUnits" />
+      </label>
+      <label>
+        <input v-model="newStatus" />
+      </label>
+    </form>
+  </div>
+</template>
