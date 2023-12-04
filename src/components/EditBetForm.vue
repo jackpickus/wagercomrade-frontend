dd7961eb3891962088e9d6b4b494df28340f5d25
Jack <jpick329@gmail.com>
2023-12-03 19:31:39 -0600

    fix html select error

--- src/components/EditBetForm.vue
+++ src/components/EditBetForm.vue
@@ -41,15 +41,20 @@ export default {
         <input v-model="newUnits" type="number" />
       </label>
       <label for="status-select"> Status </label>
-      <select name="status" v-model="newStatus">
-        <!-- TODO - change value to the Status enum values -->
-        <option value="PENDING">{{ Status.PENDING }}</option>
+
+      <select name="status" id="status-select">
+        <option value="">--Please choose an option--</option>
         <option value="WON">{{ Status.WON }}</option>
         <option value="LOST">{{ Status.LOST }}</option>
+        <option value="PENDING">{{ Status.PENDING }}</option>
         <option value="VOID">{{ Status.VOID }}</option>
       </select>
       <hr />
-      <button>Submit</button>
+      <button
+        class="md:border-2 rounded-lg m-2 p-2 text-white bg-green-600 hover:bg-violet-600"
+      >
+        Submit
+      </button>
     </form>
   </div>
 </template>
