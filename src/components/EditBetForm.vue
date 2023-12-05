fd3cffc12965ef078a87b9a98f7b610798be3bdd
Jack <jpick329@gmail.com>
2023-12-04 22:11:39 -0600

    add some style to input boxes

--- src/components/EditBetForm.vue
+++ src/components/EditBetForm.vue
@@ -24,26 +24,42 @@ export default {
     <form @submit.prevent="">
       <label>
         Wager
-        <input v-model="newBet" type="text" />
+        <input
+          v-model="newBet"
+          type="text"
+          class="border-2 rounded-xl border-zinc-500 px-2 focus:border-emerald-600 focus:outline-none"
+        />
       </label>
       <label>
         Odds
-        <input v-model="newOdds" type="text" />
+        <input
+          v-model="newOdds"
+          type="text"
+          class="border-2 rounded-xl border-zinc-500 px-2 focus:border-emerald-600 focus:outline-none"
+        />
       </label>
       <label>
         Units
-        <input v-model="newUnits" type="number" />
+        <input
+          v-model="newUnits"
+          type="number"
+          class="border-2 rounded-xl border-zinc-500 px-2 focus:border-emerald-600 focus:outline-none"
+        />
       </label>
       <label for="status-select"> Status </label>
 
-      <select name="status" id="status-select" v-model="newStatus">
+      <select
+        name="status"
+        id="status-select"
+        v-model="newStatus"
+        class="border-2 rounded-xl border-zinc-500 px-2 focus:border-emerald-600 focus:outline-none"
+      >
         <option value="">--Please choose an option--</option>
         <option value="WON">{{ Status.WON }}</option>
         <option value="LOST">{{ Status.LOST }}</option>
         <option value="PENDING">{{ Status.PENDING }}</option>
         <option value="VOID">{{ Status.VOID }}</option>
       </select>
-      <hr />
       <button
         class="md:border-2 rounded-lg m-2 p-2 text-white bg-green-600 hover:bg-violet-600"
       >
