46572e3271421540f5cf0586614695c17943d4aa
Jack <jpick329@gmail.com>
2023-12-08 17:16:28 -0600

    style login form

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -5,23 +5,34 @@ const username = ref("");
 const password = ref("");
 </script>
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="text-center">
+    <h1 class="font-bold text-4xl mb-4">Login</h1>
     <form>
-      <label>Username: <input v-model="username" /></label>
-      <label>Password:<input v-model="password" type="password" /></label>
-      <button type="submit">Login</button>
+      <div class="flex flex-col mb-4">
+        <label class="uppercase"
+          >Username
+          <input
+            v-model="username"
+            class="border-2 rounded-xl border-zinc-500 px-2 focus:border-emerald-600 focus:outline-none"
+          />
+        </label>
+      </div>
+      <div class="flex flex-col mb-6">
+        <label class="uppercase">
+          Password
+          <input
+            v-model="password"
+            type="password"
+            class="border-2 rounded-xl border-zinc-500 px-2 focus:border-emerald-600 focus:outline-none"
+          />
+        </label>
+      </div>
+      <button
+        type="submit"
+        class="mx-auto uppercase block md:border-2 rounded-lg m-2 p-2 text-white bg-green-600 hover:bg-violet-600"
+      >
+        Login
+      </button>
     </form>
   </div>
 </template>
-
-<style>
-label {
-  display: block;
-  margin-bottom: 10px;
-}
-
-input {
-  margin-left: 3px;
-}
-</style>
