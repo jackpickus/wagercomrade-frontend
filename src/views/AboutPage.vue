e0fe217cd8b72c0242e47669297dbe5d8afb0a60
Jack <jpick329@gmail.com>
2023-12-09 13:38:48 -0600

    style about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
+  <div class="max-w-4xl mx-auto">
     <h1>About</h1>
-    <p>Thank you for visiting my site!</p>
     <p>
       Here I will post all of my sports wagers and update them. The inspiration
       for this project is action networks wager tracker which is far better than
       this. To learn more about me please visit my
-      <a href="http://jackpickus.com" target="_blank">website</a>
+      <a
+        href="http://jackpickus.com"
+        target="_blank"
+        class="underline hover:text-violet-600"
+        >website</a
+      >
     </p>
   </div>
 </template>
