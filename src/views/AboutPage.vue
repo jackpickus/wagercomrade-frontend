621fc1db9bb3073f88a175cb6ae823bd96e0605b
Jack <jpick329@gmail.com>
2023-12-10 22:26:49 -0600

    update about page content

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -2,15 +2,8 @@
   <div class="max-w-4xl mx-auto">
     <h1>About</h1>
     <p>
-      Here I will post all of my sports wagers and update them. The inspiration
-      for this project is action networks wager tracker which is far better than
-      this. To learn more about me please visit my
-      <a
-        href="http://jackpickus.com"
-        target="_blank"
-        class="underline hover:text-violet-600"
-        >website</a
-      >
+      My Wager Tracker is the premier sports wager app where you can enter and
+      track all of your bets as well as your friends!
     </p>
   </div>
 </template>
