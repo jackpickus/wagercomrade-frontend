97ed5f31611d254f88634d533e6510d1d474efd7
Jack <jpick329@gmail.com>
2023-12-22 16:18:13 -0600

    make register url a const

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 
+const URL = "http://localhost:8080/api/v1/registration/";
 export const useUserStore = defineStore("UserStore", {
   state: () => ({
     user: null,
@@ -7,30 +8,24 @@ export const useUserStore = defineStore("UserStore", {
 
   actions: {
     async logIn(username, password) {
-      const res = await fetch(
-        "http://localhost:8080/api/v1/registration/authenticate",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ username, password }),
-        }
-      );
+      const res = await fetch(URL + "authenticate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
       const user = await res.json();
       this.user = user;
     },
     async register(username, email, password, passwordMatch) {
-      const res = await fetch(
-        "http://localhost:8080/api/v1/registration/register",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ username, email, password, passwordMatch }),
-        }
-      );
+      const res = await fetch(URL + "register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password, passwordMatch }),
+      });
       const user = await res.json();
       this.user = user;
     },
